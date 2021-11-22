36ea6b0777d18c40e619f42263fc79dcd20162af
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-11-22 12:26:44 -0500

    commit tree 3717332ae3b1ea040f66b4c632949e7aa5000fa8

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students_by_grade = Student.all.order('grade DESC')
+        render json: students_by_grade
+    end
+end
